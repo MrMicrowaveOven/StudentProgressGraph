ce1ae6ebb8ba8d6bc2a9908315b4cde851abb143
Benjamin <benj@zagorski.com>
2017-06-02 12:15:58 -0700

    Fixed x-axis to properly display dates

--- graph.js
+++ graph.js
@@ -15,14 +15,14 @@ function makeGraph(data) {
   var dataArray = [
     ["date"].concat(data.dates),
     ["MinTemps"].concat(data.minTemps),
-    // ["MaxTemps"].concat(data.maxTemps)
+    ["MaxTemps"].concat(data.maxTemps)
   ];
   console.log(dataArray[0]);
     c3.generate({
       bindto: '#chart',
       data: {
         x: "date",
-        x_format : '%Y%m%d',
+        xFormat : '%Y%m%d',
         columns: dataArray
       },
       axis: {
@@ -34,9 +34,9 @@ function makeGraph(data) {
         },
         x: {
           type: 'timeseries',
-          // tick: {
-          //   fit: false
-          // },
+          tick: {
+            format: '%m/%d/%y'
+          },
           label: 'Date',
           position: 'outer-middle'
         }
