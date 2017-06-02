628a3403af42912130d4fb56979b01c9c405ea89
Benjamin <benj@zagorski.com>
2017-06-02 12:31:13 -0700

    Removed comments and logs, cleaned up indents

--- graph.js
+++ graph.js
@@ -1,46 +1,31 @@
-
-
-// require.config({
-//   baseUrl: '/js',
-//   paths: {
-//     d3: "http://d3js.org/d3.v3.min"
-//   }
-// });
-
 function makeGraph(data) {
-  // require(["d3", "c3"], function(d3, c3) {
-  // console.log(data);
-
-  // console.log(data.dates);
   var dataArray = [
     ["date"].concat(data.dates),
     ["MinTemps"].concat(data.minTemps),
     ["MaxTemps"].concat(data.maxTemps)
   ];
-  console.log(dataArray[0]);
-    c3.generate({
-      bindto: '#chart',
-      data: {
-        x: "date",
-        xFormat : '%Y%m%d',
-        columns: dataArray
-      },
-      axis: {
-        y: {
-          label: { // ADD
-            text: 'Temperature (F)',
-            position: 'outer-middle'
-          }
-        },
-        x: {
-          type: 'timeseries',
-          tick: {
-            format: '%m/%d/%y'
-          },
-          label: 'Date',
+  c3.generate({
+    bindto: '#chart',
+    data: {
+      x: "date",
+      xFormat : '%Y%m%d',
+      columns: dataArray
+    },
+    axis: {
+      y: {
+        label: { // ADD
+          text: 'Temperature (F)',
           position: 'outer-middle'
         }
+      },
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%m/%d/%y'
+        },
+        label: 'Date',
+        position: 'outer-middle'
       }
-    });
-  // });
+    }
+  });
 }
--- request.js
+++ request.js
@@ -3,11 +3,7 @@ $.ajax({
   url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&stationid=GHCND:USC00040232&startdate=2017-01-30&enddate=2017-04-30&limit=1000",
   beforeSend: function(xhr){xhr.setRequestHeader('token', 'TDslBowwDzKucWUwYmhdEiaKhBLVBmDB');},
   success: function (res) {
-    // console.log("Success!");
-
-      // console.log(res.results);
-      makeArraysOfData(res.results);
-      // makeGraph(res);
+    makeArraysOfData(res.results);
   },
   error: function (xhr, status, error) {
     console.log(error);
@@ -23,15 +19,10 @@ function makeArraysOfData(data) {
   var minTemps = [];
   var maxTemps = [];
   data.forEach(function(dataPoint) {
-    // console.log(dataPoint);
     if (dataPoint.datatype == "TMAX") {
       maxTemps.push(dataPoint.value);
-      // currentDateObject["TMAX"] = dataPoint.value;
-      // currentDateObject["date"] = dataPoint.date;
     } else if (dataPoint.datatype == "TMIN") {
-      minTemps.push(dataPoint.value)
-      // currentDateObject["TMIN"] = dataPoint.value;
-      // currentDateObject["date"] = dataPoint.date;
+      minTemps.push(dataPoint.value);
     }
     if (currentDate !== dataPoint.date) {
       dates.push(currentDate.slice(0,4) + currentDate.slice(5,7) + currentDate.slice(8,10));
@@ -39,13 +30,12 @@ function makeArraysOfData(data) {
     }
   });
   dates.push(data[data.length - 1].date.slice(0,4) + data[data.length - 1].date.slice(5,7) + data[data.length - 1].date.slice(8,10));
-  // console.log(parsedData.dates);
+
   parsedData = {
     dates: dates,
     minTemps: minTemps,
     maxTemps: maxTemps,
   };
 
-  // $("#response").html(JSON.stringify(parsedDataArray));
   makeGraph(parsedData);
 }
