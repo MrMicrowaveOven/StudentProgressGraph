288176c2dce11eceffc7e37a0a9f2bc185479951
Benjamin <benj@zagorski.com>
2017-06-02 11:00:18 -0700

    Installed D3 and parsed data into convenient array

--- request.js
+++ request.js
@@ -5,9 +5,34 @@ $.ajax({
   success: function (res) {
     console.log("Success!");
       $("#response").html(JSON.stringify(res));
-      console.log(res);
+      // console.log(res.results);
+      makeArraysOfData(res.results);
+      // makeGraph(res);
   },
   error: function (xhr, status, error) {
     console.log(error);
   }
 });
+
+function makeArraysOfData(data) {
+  console.log(data);
+  var parsedData = [];
+  var currentDateObject = {};
+  var currentDate = data[0].date;
+  data.forEach(function(dataPoint) {
+    console.log(dataPoint);
+    if (dataPoint.datatype == "TMAX") {
+      currentDateObject["TMAX"] = dataPoint.value;
+      currentDateObject["date"] = dataPoint.date;
+    } else if (dataPoint.datatype == "TMIN") {
+      currentDateObject["TMIN"] = dataPoint.value;
+      currentDateObject["date"] = dataPoint.date;
+    }
+    if (currentDate !== dataPoint.date) {
+      parsedData.push(currentDateObject);
+      currentDate = dataPoint.date;
+      currentDateObject = {};
+    }
+  });
+  makeGraph(parsedData);
+}
